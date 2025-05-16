e5318d105c3abd372b00f17bbc8aca509143d1e9
javeed512 <javeedmohammedjaveed@gmail.com>
2025-05-16 17:03:32 +0530

    Scope vars,RequestDispatcher , reading form data  demos, notes,diagrams

---
+++ Servlet_Jsp_Demos/WebApplication_Demo2/src/main/java/com/hexaware/entity/DisplayServlet.java
@@ -0,0 +1,71 @@
+package com.hexaware.entity;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class DisplayServlet
+ */
+@WebServlet("/DisplayServlet")
+public class DisplayServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public DisplayServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out =	 response.getWriter();
+		
+		response.setContentType("text/html");
+		
+				out.print("<br>");
+				out.print("Welcome to DisplayServlet <br>");
+				
+				//out.print(request.getParameter("ename"));
+				
+			HttpSession session =	request.getSession();
+			
+			  out.print("<br>display session "+session);
+				
+			
+			Employee  emp  =  (Employee)		session.getAttribute("e1");
+			
+			
+			out.print(emp);
+			
+					session.removeAttribute("e1");
+					
+					
+					ServletContext  application =  	request.getServletContext();
+				
+		
+					String city =  (String)	application.getAttribute("city");
+					
+						out.print("<br>city : "+city);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Servlet_Jsp_Demos/WebApplication_Demo2/src/main/java/com/hexaware/web/WelcomeServlet.java
@@ -0,0 +1,50 @@
+package com.hexaware.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public WelcomeServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+			PrintWriter out =	response.getWriter();
+			
+					out.print("Hello friends!");
+					
+					out.print("<h1 style='color:red' >Welcome to Servlets</h1>");
+					
+					response.setContentType("text/html");
+					  
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
