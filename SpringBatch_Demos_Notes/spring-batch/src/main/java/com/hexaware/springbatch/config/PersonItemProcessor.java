3de57d1e3abd5541e4768ef052559d4b266e35ce
javeed512 <javeedmohammedjaveed@gmail.com>
2025-05-27 14:12:02 +0530

    Springbatch demo,notes

---
+++ SpringBatch_Demos_Notes/spring-batch/src/main/java/com/hexaware/springbatch/config/PersonItemProcessor.java
@@ -0,0 +1,17 @@
+package com.hexaware.springbatch.config;
+
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.hexaware.springbatch.entity.Person;
+
+public class PersonItemProcessor implements ItemProcessor<Person, Person> {
+
+    @Override
+    public Person process(Person person) throws Exception {
+        String firstName = person.getFirstName().toUpperCase();
+        String lastName = person.getLastName().toUpperCase();
+
+        return new Person(firstName, lastName);
+    }
+}
