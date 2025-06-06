2d8a27009cecf6595bb750613b3ab6c585700ef6
javeed512 <javeedmohammedjaveed@gmail.com>
2025-06-06 11:31:00 +0530

    routing demos , pdf , diagrams

---
+++ Angular_Projects_Notes/Angular_Day4/src_routing_fundamentals/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'webapp';
+
+
+      constructor(private router:Router){
+
+      }
+
+
+          go(){
+
+            this.router.navigate(['/aboutus']);
+
+
+
+
+          }
+
+
+
+}
